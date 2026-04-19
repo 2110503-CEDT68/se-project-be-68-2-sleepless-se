25221374721fe1c3d6c99c25a806ed8c5ef1b1cd
ppawanktm <pawanjakkaew24@gmail.com>
2026-04-19 18:41:14 +0700

    addshcema

--- models/Hotel.js
+++ models/Hotel.js
@@ -31,7 +31,23 @@ const HotelSchema = new mongoose.Schema({
     region:{
         type: String,
         required: [true, 'Please add a region']
+    },
+      description: {
+        type: String,
+        maxlength: [500, 'Description cannot be more than 500 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a valid email'
+        ]
+    },
+    imageURL: {
+        type: String,
+        default: 'no-photo.jpg'
     }
+
 }, {
     toJSON:{virtuals:true},
     toObject:{virtuals:true}
