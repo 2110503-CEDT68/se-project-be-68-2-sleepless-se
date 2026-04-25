e227fe0532d68ee21b20fe7e4fc8c2b3eae75b98
meenthioneth <peampimon2550@gmail.com>
2026-04-26 01:25:24 +0700

    fix code

--- server.js
+++ server.js
@@ -43,11 +43,8 @@ app.use('/api/v1/auth', auth);
 app.use('/api/v1/bookings', bookings);
 app.use('/api/v1/admin', admin);
 app.use('/api/v1/hotels/:hotelId/reviews', reviews);
+app.use('/api/v1/reviews', reviews);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running...'));
 
-const reviews = require('./routes/reviews');
-app.use('/api/v1/hotels/:hotelId/reviews', reviews);
-
-app.use('/api/v1/reviews', reviews);
